d6dde0177c899a18837ac45c41525812de995671
brunamarton <contatobrunab@hotmail.com>
2019-05-27 22:12:16 -0300

    sexto commit

--- database/migrations/2019_05_28_004810_create_products_table.php
+++ database/migrations/2019_05_28_004810_create_products_table.php
@@ -4,28 +4,22 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateProductsTable extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
+class CreateProductsTable extends Migration {
+
+    public function up() {
         Schema::create('products', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('nome', 42);
+            $table->integer('estoqueMaximo');
+            $table->integer('estoqueMinimo');
+            $table->double('precoCompra', 8, 2);
+            $table->double('precoVenda', 8, 2);
+            $table->enum('status', array('ATIVO', 'INATIVO'));
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
+    public function down() {
         Schema::dropIfExists('products');
     }
 }
